88c96a43d73dac5f2ac449ec40cb9db669547f89
Dongoug <dougouk@gmail.com>
2017-02-04 15:26:51 -0500

    Merging

---
+++ MyApplication/app/src/main/java/com/leoybkim/walkingbuddy/MapActivity.java
@@ -0,0 +1,21 @@
+package com.leoybkim.walkingbuddy;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.android.gms.maps.*;
+
+/**
+ * Created by Dan on 04/02/2017.
+ */
+
+public class MapActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.map_activity);
+
+
+    }
+}
