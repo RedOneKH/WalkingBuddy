7998081d7f489aca242f166402730317ad9067c2
Daniel Medina <danielmedinagarcia@protonmail.com>
2017-02-04 18:49:39 -0500

    Cards!

--- MyApplication/app/src/main/java/com/leoybkim/walkingbuddy/BuddyMatcher/BuddyFoundActivity.java
+++ MyApplication/app/src/main/java/com/leoybkim/walkingbuddy/BuddyMatcher/BuddyFoundActivity.java
@@ -1,11 +1,12 @@
 package com.leoybkim.walkingbuddy.BuddyMatcher;
 
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.widget.ImageButton;
-import android.widget.TextView;
 
+import com.leoybkim.walkingbuddy.Buddy;
 import com.leoybkim.walkingbuddy.R;
 
 /**
@@ -14,22 +15,30 @@ import com.leoybkim.walkingbuddy.R;
 
 public class BuddyFoundActivity extends AppCompatActivity {
 
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        // call the super class onCreate to complete the creation of
-        // activity like the view hierarchy
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        // set the user interface layout for this Activity
-        // the layout file is defined in the project res/layout/buddy_found.xml file
         setContentView(R.layout.buddy_found);
+        mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
 
-        // Instantiate the object in the view
-        RecyclerView recycler = findViewById(R.id.recycler_view);
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
 
-        recycler.a
+        // use a linear layout manageri
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        Buddy testbuddy = new Buddy("sdf", "Dani Medina", null);
+        Buddy testbuddy2 = new Buddy("sdf", "Dan Kim", null);
 
+        // specify an adapter (see also next example)
+        Buddy[] dataset = { testbuddy, testbuddy2 };
+        mAdapter = new CardAdapter(dataset);
+        mRecyclerView.setAdapter(mAdapter);
     }
-//    TODO: listeners
 
 }
--- MyApplication/app/src/main/java/com/leoybkim/walkingbuddy/BuddyMatcher/ImageCardAdapter.java
+++ MyApplication/app/src/main/java/com/leoybkim/walkingbuddy/BuddyMatcher/ImageCardAdapter.java
@@ -1,68 +1,62 @@
 package com.leoybkim.walkingbuddy.BuddyMatcher;
 
+import android.graphics.drawable.Drawable;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
-import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.TextView;
 
-import com.leoybkim.walkingbuddy.Buddy;
 import com.leoybkim.walkingbuddy.R;
-
 
 /**
  * Created by dmedinag on 04/02/2017.
  */
 
-public class CardAdapter extends RecyclerView.Adapter<CardAdapter.ViewHolder> {
-    private Buddy[] mDataset;
+public class ImageCardAdapter extends RecyclerView.Adapter<ImageCardAdapter.ImageViewHolder> {
+    private Drawable mDataset;
 
     // Provide a reference to the views for each data item
     // Complex data items may need more than one view per item, and
     // you provide access to all the views for a data item in a view holder
-    public static class ViewHolder extends RecyclerView.ViewHolder {
-        // each data item is just a string in this case
-        public TextView mName;
-        public ImageView mProfilePic;
 
-        public ViewHolder(View v) {
+    public static class ImageViewHolder extends RecyclerView.ViewHolder {
+        // each data item is just a string in this case
+        public ImageView mImageView;
+        public ImageViewHolder(ImageView v) {
             super(v);
-            mName = (TextView) v.findViewById(R.id.buddyName);
-            mProfilePic = (ImageView) v.findViewById(R.id.buddyPic);
+            mImageView = v;
         }
     }
 
     // Provide a suitable constructor (depends on the kind of dataset)
-    public CardAdapter(Buddy[] dataset) {
+    public ImageCardAdapter(Drawable dataset) {
         mDataset = dataset;
     }
 
     // Create new views (invoked by the layout manager)
     @Override
-    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        // create a new view
-        View v = LayoutInflater.from(parent.getContext())
+    public ImageViewHolder onCreateViewHolder(ViewGroup parent, int position) {
+        ImageView v = (ImageView) LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.buddy_card, parent, false);
 
-        ViewHolder vh = new ViewHolder(v);
+        ImageViewHolder vh = new ImageViewHolder(v);
         return vh;
     }
 
 
+
     // Replace the contents of a view (invoked by the layout manager)
     @Override
-    public void onBindViewHolder(ViewHolder holder, int position) {
+    public void onBindViewHolder(ImageViewHolder holder, int position) {
         // - get element from your dataset at this position
         // - replace the contents of the view with that element
-        holder.mName.setText(mDataset[position].getName());
-        holder.mProfilePic.setImageDrawable(mDataset[position].getBuddyPic());
+        holder.mImageView.setImageDrawable(mDataset);
+
     }
 
     // Return the size of your dataset (invoked by the layout manager)
     @Override
     public int getItemCount() {
-        return mDataset.length;
+        return 1;
     }
 }
-
